e99e3ca19e656ad147c877d7694b5249378e790f
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-10-24 13:48:59 +0200

    Edit comment and like entities

--- db/migrate/20231024032549_create_comments.rb
+++ db/migrate/20231024032549_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.references :author, foreign_key: { to_table: :users }
+      t.references :user, foreign_key: true
       t.references :post, foreign_key: true
       t.text :text
 
