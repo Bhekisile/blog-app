a2327c7d45f81fe437e5930cdb43eddb1499ac6d
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-10-25 03:43:55 +0200

    Create model and set up associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name :user, foreign_key: :author_id
+  has_many :comments
+  has_many :likes
+
+  after_create :update_user_posts_counter
+
+  private
+
+  def update_user_posts_counter
+    author.update(posts_counter :author.posts.count)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
