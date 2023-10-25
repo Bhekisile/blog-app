338d69b6ee448466621fac9cd90ecc60b42df566
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-10-25 11:35:50 +0200

    Create custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,6 @@ class Comment < ApplicationRecord
 
   after_create :update_post_comments_counter
 
-  private
-
   def update_post_comments_counter
     post.update(comments_counter: post.comments.count)
   end
