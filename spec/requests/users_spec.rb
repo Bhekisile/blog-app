f58c57606ab998ad36a8c54fb3a7b3f3ef9f7635
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-10-31 10:22:23 +0200

    Create a Request spec file for users

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    it 'returns a successful response' do
+      get users_path
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'renders the correct placeholder text in the response body' do
+      get users_path
+      expect(response.body).to include('Here is the user')
+    end
+  end
+
+  describe 'GET /show' do
+    user = User.create!(name: 'Zenaye', posts_counter: 0 )
+    it 'renders a successful response' do
+      get user_url(user)
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      get user_url(user)
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders the correct placeholder text in the response body' do
+      get user_url(user)
+      expect(response.body).to include('Users details')
+    end
+  end
+end
