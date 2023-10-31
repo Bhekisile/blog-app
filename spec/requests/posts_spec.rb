9f645420078fb59f3f8efb7a0271d70386699486
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-10-31 10:22:53 +0200

    Create a Request spec file for posts

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    user = User.create!(name: 'Zenaye', posts_counter: 0 )
+    it 'returns a successful response' do
+      get user_posts_path(user)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      get user_posts_path(user)
+      expect(response).to render_template(:index)
+    end
+
+    it 'renders the correct placeholder text in the response body' do
+      get user_posts_path(user)
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
+
+  describe 'GET /show' do
+    user = User.create!(name: 'Zenaye', posts_counter: 0 )
+    post = Post.create!(title: 'Title 1', text: 'First post', comments_counter: 0, likes_counter: 0,
+      author_id: user.id )
+    it 'renders a successful response' do
+      get user_post_path(user, post)
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      get user_post_path(user, post)
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders the correct placeholder text in the response body' do
+      get user_post_path(user, post)
+      expect(response.body).to include('Posts details')
+    end
+  end
+end
