d52fb88f0d9402fabf706c68e32c7f1bf3bb6c8c
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-11-01 23:14:40 +0200

    Replace user_id with author_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_032905) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_01_203959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer "author_id"
     t.integer "post_id"
     t.text "text"
     t.datetime "created_at", null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_032905) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer "author_id"
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
