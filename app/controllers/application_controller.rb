82493d3097eb00ba922402755477edee16d80f94
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-11-09 10:03:46 +0330

    Add 2 methods to the application_controller class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::Base
-  before_action :current_user
-
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
   helper_method :current_user
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def after_sign_out_path_for(_resources_or_scope)
+    # Replace '/your_custom_path' with the path you want to redirect to after sign_out
+    '/users/sign_in'
+  end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :photo, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :email, :password, :current_password)
+    end
+  end
 end
