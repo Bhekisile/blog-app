d55df151ede0e69a808de53586fd14919438bfcc
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-11-10 10:54:36 +0200

    Edit delete button for post and comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,21 +30,43 @@ class PostsController < ApplicationController
   # def destroy
   #   @post = Post.find(params[:id])
   #   @post.destroy
-  #   redirect_to user_posts_path(@post.author), notice: 'Post was deleted.'
-  # redirect_to user_posts_path(@user), notice: 'Post was deleted.'
+  #   redirect_to user_path(@User), notice: 'Post was deleted.'
+  # end
+
+  # def destroy
+  #   @user = User.find(params[:user_id])
+  #   @post = Post.find(params[:id])
+  #   @post.destroy
+  #   respond_to(&:turbo_stream)
+  #   redirect_to user_posts_path(@users)
   # end
 
   def destroy
-    @post = Post.find(params[:id])
-    @user = @post.author
-    @post.destroy
-      redirect_to user_path(@user), notice: 'Post was deleted.'
+    @user = User.find(params[:user_id])
+    post = Post.find(params[:id])
+    authorize! :destroy, post
+
+    post.comments.destroy_all
+    post.likes.destroy_all
+    post.destroy
+    respond_to(&:turbo_stream)
+
+    if post.destroy
+      flash[:success] = 'Post deleted successfully'
+      redirect_to user_posts_path(@user)
     else
-      redirect_to user_path(@user), alert: 'Error deleting the post.'
+      flash.now[:error] = 'Error: Post could not be deleted'
+      redirect_to user_post_path(@user, post)
     end
-  rescue ActiveRecord::InvalidForeignKey
-    redirect_to user_path(@user), alert: 'Error deleting the post: There are associated comments.'
   end
+
+  # def destroy
+  #   @post.destroy!
+  #   respond_to do |format|
+  #     redirect_to user_post_path(@user, post), notice: 'Post deleted successfully'
+      # format.json { head :no_content }
+  #   end
+  # end
 
   private
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,6 @@ class Comment < ApplicationRecord
   after_destroy :update_post_comments_counter
 
   def update_post_comments_counter
-    post.increment!(:comments_counter)
+    post.update(comments_counter: post.comments.count)
   end
 end
